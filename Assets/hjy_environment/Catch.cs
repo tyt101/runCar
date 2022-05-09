5f6ff1b78a1b9913e5298d5a8bc99d33a03aedb4
maziike <292586108@qq.com>
2022-05-09 15:38:04 +0800

    hjy..

--- Assets/hjy_environment/Catch.cs
+++ Assets/hjy_environment/Catch.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;//引入unity的UI编辑库
 
 public class Catch : MonoBehaviour
 {
-    public Move player;//定义PlayerControl类
+    public Text txtcoin;
+    void Update()
+    {
+        GameObject ifCollect = GameObject.Find("/Canvas/Money/ScoreText");
+        ifCollect.GetComponent<Text>().text = Coin.Money.ToString();
+    }
+  
+    /*public Move Car;//定义PlayerControl类
     public int score;//定义积分变量
     public Text ScoreText;//定义要修改的Text
     void Start()
     {
-        player.GetScore += Player_GetScore;//调用PlayerControl类，订阅Player的得分事件，start函数已经作为了我们的消息接收器
+        Car.GetScore += Player_GetScore;//调用类，订阅Player的得分事件，start函数已经作为了我们的消息接收器
     }
 
     private void Player_GetScore(int score)//定义消息处理器来处理消息，给属性赋值，改变积分值
@@ -32,6 +39,6 @@ public class Catch : MonoBehaviour
             score = value;
             ScoreText.text = score.ToString();//让UI显示的分数等于score的值，这里有String类型的转换
         }
-    }
+    }*/
 
 }
